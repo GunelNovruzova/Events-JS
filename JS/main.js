880e84b6a37c904651e759ead4dc23a68e3c222e
GunelNovruzova <gunelvn@code.edu.az>
2022-02-22 11:50:06 -0800

    first

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+
+//Task1
+    
+   function Delete(){
+       var str=document.getElementById('inp').value;
+       str=str.slice(0,-1);
+       document.getElementById('inp').value=str;
+
+   }
+//Task2
+   function Change(){
+    document.body.setAttribute('class', 'active');
+   }
+   
